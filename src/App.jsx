8e6df412851db5b97789e1cf9627875b3826d2cb
Naufal Irawan <irawanaufal29209@gmail.com>
2023-11-16 15:42:58 +0700

    fix sidebar - sama ratain map

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     <>
     <div className='flex mr-auto'>
       <Sidebar />
-      <div className="w-[80vw] h-80%">
+      <div className="w-[85vw] h-80%">
         <MapContainer center={[-6.761946739265074, 107.20976353938751]} zoom={13} className='w-full'>
           <TileLayer attribution="&copy; <a>Kelompok 2</a>" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
