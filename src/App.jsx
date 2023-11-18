d53d6912de679d4a39748b7240f9503e0a91c514
Naufal Irawan <irawanaufal29209@gmail.com>
2023-11-18 16:22:48 +0700

    polyline map tracking each device

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Sidebar from "../src/component/Sidebar"
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Popup, Marker} from 'react-leaflet';
+import { MapContainer, TileLayer, Popup, Marker, Polyline } from 'react-leaflet';
 import axios from 'axios';
-
 
 function App() {
   const [data, setData] = useState([]);
@@ -13,40 +12,57 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:8080/data/getAll');
-        setData(response.data); // Assuming the data is an array
+        setData(response.data);
       } catch (error) {
         console.error(error);
       }
     };
 
-    console.log(data)
     fetchData();
-  }, []); 
+  }, []);
+
+  // Group points by Name
+  const groupedData = {};
+  data.forEach(titik => {
+    if (!groupedData[titik.Name]) {
+      groupedData[titik.Name] = [];
+    }
+    groupedData[titik.Name].push([titik.Lattitude, titik.Longitude]);
+  });
 
   return (
     <>
-    <div className='flex h-screen w-screen'>
-      <Sidebar />
-      <div className="h-auto w-full">
-        <MapContainer center={[-6.761946739265074, 107.20976353938751]} zoom={13} className=''>
-          <TileLayer attribution="&copy; <a>Kelompok 2</a>" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <div className='flex h-screen w-screen'>
+        <Sidebar />
+        <div className="h-auto w-full">
+          <MapContainer center={[-6.760105360614504, 107.20951551421041]} zoom={50} className=''>
+            <TileLayer attribution="&copy; <a>Kelompok 2</a>" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-          {data.map(titik =>(
-            <Marker position={[titik.Lattitude, titik.Longitude]}>
-              <Popup>
-                {titik.Name}
-                <br/>
-                {titik.Lattitude}
-                <br/>
-                {titik.Longitude}
-                </Popup>
-            </Marker>
-          ))
+            {Object.entries(groupedData).map(([name, polylinePoints], index) => (
+              <div key={index}>
+                <h2>{name}</h2>
+                {polylinePoints.length > 1 && (
+                  <Polyline key={index} positions={polylinePoints} color="blue" />
+                )}
 
-          }
-        </MapContainer>
+                {data
+                  .filter(titik => titik.Name === name)
+                  .map(titik => (
+                    <Marker key={titik.id} position={[titik.Lattitude, titik.Longitude]}>
+                      <Popup>
+                        {titik.Name}
+                        <br />
+                        {titik.Lattitude}
+                        <br />
+                        {titik.Longitude}
+                      </Popup>
+                    </Marker>
+                  ))}
+              </div>
+            ))}
+          </MapContainer>
+        </div>
       </div>
-    </div>
     </>
   );
 }
