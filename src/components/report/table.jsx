5a41a83a1ba9e5896918d89249d8c2eaafcfdb7e
Naufal Irawan <irawanaufal29209@gmail.com>
2024-02-12 00:29:57 +0700

    ganti table ke table biasa (delete datatables)

--- src/components/report/table.jsx
+++ src/components/report/table.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import DataTable from 'react-data-table-component';
 import { DocumentIcon, PencilSquareIcon, TrashIcon } from '@heroicons/react/24/solid';
 
 function Table() {
@@ -11,26 +10,25 @@ function Table() {
   const [newStatus, setNewStatus] = useState('');
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const [deleteItemId, setDeleteItemId] = useState(null);
-
   const [newCatatan, setNewCatatan] = useState('');
 
   const handleUpdateCatatan = async () => {
-      try {
-          const response = await axios.put(
-              `http://localhost:8080/data/updateCatatan/${selectedItem.ID}`,
-              {
-                  catatan: newCatatan,
-              }
-          );
-          console.log(`Updated catatan for ID ${selectedItem.ID}`);
-          const updatedData = data.map((item) =>
-              item.ID === selectedItem.ID ? { ...item, Catatan: newCatatan } : item
-          );
-          setData(updatedData);
-          closeCatatanModal();
-      } catch (error) {
-          console.error(error);
-      }
+    try {
+      const response = await axios.put(
+        `http://localhost:8080/data/updateCatatan/${selectedItem.ID}`,
+        {
+          catatan: newCatatan,
+        }
+      );
+      console.log(`Updated catatan for ID ${selectedItem.ID}`);
+      const updatedData = data.map((item) =>
+        item.ID === selectedItem.ID ? { ...item, Catatan: newCatatan } : item
+      );
+      setData(updatedData);
+      closeCatatanModal();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const fetchData = async () => {
@@ -116,57 +114,48 @@ function Table() {
     setShowCatatanModal(false);
   };
 
-  const columns = [
-    { name: 'ID', selector: 'ID', sortable: true, width: '8%' },
-    { name: 'Time', selector: 'Time', sortable: true, width: '15%' },
-    { name: 'Name', selector: 'Name', sortable: false, width: '8%' },
-    { name: 'Latitude', selector: 'Lattitude', sortable: false, width: '10%' },
-    { name: 'Longitude', selector: 'Longitude', sortable: false, width: '10%' },
-    { name: 'Status', selector: 'Status', sortable: false, width: '10%' },
-    { name: 'Catatan', selector: 'Catatan', sortable: false, width: '25%',
-      cell: (row) => (
-        <div style={{ maxHeight: '100px', overflowY: 'auto' }}>
-          {row.Catatan}
-        </div>
-      ),
-    },
-    {
-      name: 'Action', width: '15%',
-      cell: (row) => (
-        <div>
-          <button onClick={() => openStatusModal(row)}>
-            <PencilSquareIcon className="h-7 w-7 p-1 bg-blue-600 rounded-md hover:bg-blue-300" />
-          </button>
-          <button onClick={() => openCatatanModal(row)}>
-            <DocumentIcon className="h-7 w-7 p-1 bg-yellow-600 rounded-md hover:bg-yellow-300" />
-          </button>
-          <button onClick={() => handleDelete(row.ID)}>
-            <TrashIcon className="h-7 w-7 p-1 bg-red-600 rounded-md hover:bg-red-300" />
-          </button>
-        </div>
-      ),
-    },
-  ];
-
   return (
     <div className="container mx-auto p-5 align-middle">
-      <DataTable
-        title="Tabel Tracking"
-        columns={columns}
-        data={data}
-        pagination
-        highlightOnHover
-        striped
-        noHeader
-        className="bg-white shadow-md rounded-md"
-        customStyles={{
-          headCells: {
-            style: {
-              textAlign: 'center',
-            },
-          },
-        }}
-      />
+      <table className="table">
+        <thead>
+          <tr className="bg-red-500 border-b-2 border-gray-200 text-black">
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '10%' }}>ID</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '15%' }}>Time</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '20%' }}>Name</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '15%' }}>Latitude</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '15%' }}>Longitude</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '15%' }}>Status</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '25%' }}>Catatan</th>
+          <th className="p-3 text-sm font-semibold tracking-wide" style={{ width: '10%' }}>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.ID} className="odd:bg-blue-100 even:bg-blue-300">
+              <td className="p-3 text-sm text-black text-center">{row.ID}</td>
+              <td className="p-3 text-sm text-black text-center">{row.Time}</td>
+              <td className="p-3 text-sm text-black text-center">{row.Name}</td>
+              <td className="p-3 text-sm text-black text-center">{row.Lattitude}</td>
+              <td className="p-3 text-sm text-black text-center">{row.Longitude}</td>
+              <td className="p-3 text-sm text-black text-center">{row.Status}</td>
+              <td className="p-3 text-sm text-black text-center">
+                <div style={{ maxHeight: '100px', overflowY: 'auto' }}>{row.Catatan}</div>
+              </td>
+              <td className="p-3 text-sm text-black text-center">
+                <button onClick={() => openStatusModal(row)}>
+                  <PencilSquareIcon className="h-7 w-7 p-1 bg-blue-600 rounded-md hover:bg-blue-300" />
+                </button>
+                <button onClick={() => openCatatanModal(row)}>
+                  <DocumentIcon className="h-7 w-7 p-1 bg-yellow-600 rounded-md hover:bg-yellow-300" />
+                </button>
+                <button onClick={() => handleDelete(row.ID)}>
+                  <TrashIcon className="h-7 w-7 p-1 bg-red-600 rounded-md hover:bg-red-300" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
       {/* Modal for updating status */}
       {showStatusModal && (
@@ -200,7 +189,6 @@ function Table() {
         </div>
       )}
 
-
       {/* Confirmation Modal for delete */}
       {showDeleteConfirmation && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
@@ -227,28 +215,28 @@ function Table() {
       {/* Modal for updating Catatan */}
       {showCatatanModal && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
-            <div className="bg-white p-8 rounded-md">
-                <label className="block mb-2 text-sm font-semibold text-gray-600">Update Catatan</label>
-                <textarea
-                    className="w-full p-2 mb-4 border rounded-md"
-                    value={newCatatan}
-                    onChange={(e) => setNewCatatan(e.target.value)}
-                />
-                <button
-                    onClick={handleUpdateCatatan}
-                    className="w-full p-2 bg-yellow-600 text-white rounded-md hover:bg-yellow-300"
-                >
-                    Ya
-                </button>
-                <button
-                    onClick={closeCatatanModal}
-                    className="w-full p-2 bg-gray-400 text-white rounded-md hover:bg-gray-300 mt-2"
-                >
-                    Batal
-                </button>
-            </div>
+          <div className="bg-white p-8 rounded-md">
+            <label className="block mb-2 text-sm font-semibold text-gray-600">Update Catatan</label>
+            <textarea
+              className="w-full p-2 mb-4 border rounded-md"
+              value={newCatatan}
+              onChange={(e) => setNewCatatan(e.target.value)}
+            />
+            <button
+              onClick={handleUpdateCatatan}
+              className="w-full p-2 bg-yellow-600 text-white rounded-md hover:bg-yellow-300"
+            >
+              Ya
+            </button>
+            <button
+              onClick={closeCatatanModal}
+              className="w-full p-2 bg-gray-400 text-white rounded-md hover:bg-gray-300 mt-2"
+            >
+              Batal
+            </button>
+          </div>
         </div>
-    )}
+      )}
     </div>
   );
 }
